5d026999ce5b0f49f01a3fde1ba6589be826e9c2
Fabricio Canepa <fabri.caspel@gmail.com>
2023-09-20 23:35:01 -0300

    vinculo movies con details

--- js/admin/details.js
+++ js/admin/details.js
@@ -1,45 +1,103 @@
 import { getMoviesFromLS, getseriesFromLS } from './adminUtils.js';
 
 
-// Obtiene el código de película o serie almacenado en LS (de pagina principal)
-const searchedCode = JSON.parse(localStorage.getItem('codigoBuscado'));
+// // 
+// const cardFilmElements = document.querySelectorAll('.cardFilm');
 
+// // 
+// cardFilmElements.forEach(card => {
+//   card.addEventListener('click', () => {
+//     // obtengo el índice de la película clicada 
+//     const movieIndex = Array.from(cardFilmElements).indexOf(card);
 
-// Obtiene el array de películas y series desde LS
-const movies = getMoviesFromLS();
-const series = getseriesFromLS();
+//     // Obtiene el código de película de localStorage
+//     const movieCode = allContent[movieIndex].code;
 
-// Combina los arrays de películas y series en uno solo.
-const allContent = [...movies, ...series];
-
-
-const foundContent = allContent.find(item => item.code) ;
-
-
-
-if (foundContent) {
-  const imagenElement = document.getElementById('m-image');
-  const nameElement = document.getElementById('m-name');
-  const categoryElement = document.getElementById('m-category');
-  const descriptionElement = document.getElementById('m-description');
-  const buttonError = document.getElementById("m-btn")
+//     window.location.href = `../pages/details.html`;
+//   });
+// });
 
 
 
-  imagenElement.src = foundContent.image;
-  nameElement.innerText = foundContent.name;
-  categoryElement.innerText = foundContent.category;
-  descriptionElement.innerText = foundContent.description;
+// // Obtiene el array de películas y series desde LS
+// const movies = getMoviesFromLS();
+// const series = getseriesFromLS();
 
-  buttonError.addEventListener('click', () => {
-    window.location.href = '../pages/error404.html';
+// // Combina los arrays de películas y series en uno solo.
+// const allContent = [...movies, ...series];
+
+
+// const foundContent = allContent.find(item => item.code === movieCode) ;
+
+
+
+// if (foundContent) {
+//   const imagenElement = document.getElementById('m-image');
+//   const nameElement = document.getElementById('m-name');
+//   const categoryElement = document.getElementById('m-category');
+//   const descriptionElement = document.getElementById('m-description');
+//   const buttonError = document.getElementById("m-btn")
+
+
+
+//   imagenElement.src = foundContent.image;
+//   nameElement.innerText = foundContent.name;
+//   categoryElement.innerText = foundContent.category;
+//   descriptionElement.innerText = foundContent.description;
+
+//   buttonError.addEventListener('click', () => {
+//     window.location.href = '../pages/error404.html';
+//   });
+
+// } else {
+//   Swal.fire({
+//     icon: 'error',
+//     title: 'Oops...',
+//     text: 'Tú pelicula/serie no se encontró!',
+//   })
+
+// }
+
+
+// ...
+
+// Recorre todas las cards para dps sacar su indice
+cardFilmElements.forEach(card => {
+  card.addEventListener('click', () => {
+
+    const movieIndex = Array.from(cardFilmElements).indexOf(card);
+
+    // Busco el index que coincida con el que fue clicleado y saco el code
+    const movieCode = allContent[movieIndex].code;
+
+    const foundContent = allContent.find(item => item.code === movieCode);
+
+    if (foundContent) {
+      const imagenElement = document.getElementById('m-image');
+      const nameElement = document.getElementById('m-name');
+      const categoryElement = document.getElementById('m-category');
+      const descriptionElement = document.getElementById('m-description');
+      const buttonError = document.getElementById("m-btn");
+
+      imagenElement.src = foundContent.image;
+      nameElement.innerText = foundContent.name;
+      categoryElement.innerText = foundContent.category;
+      descriptionElement.innerText = foundContent.description;
+
+      buttonError.addEventListener('click', () => {
+        window.location.href = '../pages/error404.html';
+      });
+
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Tu película/serie no se encontró!',
+      });
+    }
+
+    window.location.href = `../pages/details.html`;
   });
+});
 
-} else {
-  Swal.fire({
-    icon: 'error',
-    title: 'Oops...',
-    text: 'Tú pelicula/serie no se encontró!',
-  })
-
-}
+// ...
